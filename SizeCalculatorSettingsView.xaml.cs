dab3abca11993f8cdc4c936372a712abf3e39596
Gerren <voldrichv@gmail.com>
2020-06-05 17:46:19 +0200

    Added link to GitHub

--- SizeCalculatorSettingsView.xaml.cs
+++ SizeCalculatorSettingsView.xaml.cs
@@ -21,5 +21,11 @@ namespace SizeCalculator
         {
             InitializeComponent();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
     }
 }
